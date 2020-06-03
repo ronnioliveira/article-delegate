8d0131d30b0d373f1313f6dae8e991c46d3b6c69
Ronni Oliveira <ronnioliveira@hotmail.com>
2020-06-03 12:05:26 -0300

    First commit with delegate project

---
+++ Delegate/AritmeticService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    public class AritmeticService
+    {
+        public static int SumTwoIntNumbers(int numberOne, int numberTwo) => numberOne + numberTwo;
+        public static int SubtractTwoIntNumbers(int numberOne, int numberTwo) => numberOne - numberTwo;
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegate
+{
+    //Criação de um delegate que retorna um inteiro e recebe dois inteiros como parâmetro.
+    public delegate int AritmeticOperations(int numberOne, int numberTwo);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOne = 7;
+            var numberTwo = 3;
+
+            var sum = AritmeticService.SumTwoIntNumbers(numberOne, numberTwo);
+            var subtraction = AritmeticService.SubtractTwoIntNumbers(numberOne, numberTwo);
+
+            Console.WriteLine($"A soma dos números é: {sum}");
+            Console.WriteLine($"A subtração dos números é: {subtraction}");
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+
+            //O delegate sumOperation é uma referência para a função SumTwoIntNumber da classe AritmeticService
+            AritmeticOperations sumOperation = AritmeticService.SumTwoIntNumbers;
+
+            //O delegate subtractOperation é uma referência para a função SubtractTwoIntNumbers da classe AritmeticService
+            AritmeticOperations subtractOperation = AritmeticService.SubtractTwoIntNumbers;
+
+            //como eu tenho a referência da função, basta invocá-la passando seus parâmetros, se houver.
+            var resultSumOperation = sumOperation.Invoke(numberOne, numberTwo);
+            var resultSubtractOperation = subtractOperation.Invoke(numberOne, numberTwo);
+
+            Console.WriteLine($"A soma dos números com delegate é: {resultSumOperation}");
+            Console.WriteLine($"A subtração dos números com delegate é: {resultSubtractOperation}");
+            
+            Console.ReadLine();
+        }
+    }
+}
+
